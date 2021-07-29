1e330cfedd0111c15592ca27be99dd33525875b4
Arjen <arjen@network4all.nl>
2021-07-29 14:52:03 +0200

    test123

--- office365urltest.go
+++ office365urltest.go
@@ -19,7 +19,7 @@ import (
 const (
 	dnsserver = "1.1.1.1"
 	clientid  = "b10c5ed1-bad1-445f-b386-b919946339a7" // request id from microsoft!
-	testvar   = "hoi"
+	testvar   = "hoi-test2"
 )
 
 type service struct {
